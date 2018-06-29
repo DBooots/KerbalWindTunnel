72826e7c9bb9ff4019f7df17e6554fc16fffbb94
David <david.boots@gmail.com>
2018-06-28 23:26:17 -0500

    Add IGraphableProvider interface for fetching graphs by name

--- Wind Tunnel/Wind Tunnel/DataGenerators/GraphGenerator.cs
+++ Wind Tunnel/Wind Tunnel/DataGenerators/GraphGenerator.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KerbalWindTunnel.Graphing;
 
 namespace KerbalWindTunnel.DataGenerators
 {
-    public abstract class DataSetGenerator : ICalculationManager, IDisposable
+    public abstract class DataSetGenerator : IGraphableProvider, ICalculationManager, IDisposable
     {
+        protected Dictionary<string, IGraphable> graphs = new Dictionary<string, IGraphable>(StringComparer.InvariantCultureIgnoreCase);
+        public virtual List<IGraphable> Graphables { get { return graphs.Values.ToList(); } }
         protected CalculationManager calculationManager = new CalculationManager();
         protected bool valuesSet = false;
 
@@ -18,6 +23,13 @@ namespace KerbalWindTunnel.DataGenerators
                     return CalculationManager.RunStatus.Completed;
                 return status;
             }
+        }
+
+        public virtual IGraphable GetGraphableByName(string graphName)
+        {
+            if (graphs.TryGetValue(graphName, out IGraphable graph))
+                return graph;
+            return null;
         }
 
         public virtual float PercentComplete
