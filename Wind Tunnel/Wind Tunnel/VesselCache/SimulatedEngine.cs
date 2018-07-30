be10e4dbc6a665417c6a6e97176f30dcbcb04a8b
David <david.boots@gmail.com>
2018-07-30 15:35:19 -0500

    Fix engine performance calculations.

--- Wind Tunnel/Wind Tunnel/VesselCache/SimulatedEngine.cs
+++ Wind Tunnel/Wind Tunnel/VesselCache/SimulatedEngine.cs
@@ -13,11 +13,18 @@ namespace KerbalWindTunnel.VesselCache
 
         public SimulatedVessel vessel;
 
+        public float flameoutBar;
         public bool atmChangeFlow;
         public bool useAtmCurve;
+        public bool useAtmCurveIsp;
         public FloatCurve atmCurve;
+        public FloatCurve atmCurveIsp;
         public bool useVelCurve;
+        public bool useVelCurveIsp;
         public FloatCurve velCurve;
+        public FloatCurve velCurveIsp;
+        public bool useThrustCurve;
+        public FloatCurve thrustCurve;
         public float flowMultCap;
         public float flowMultCapSharpness;
         public FloatCurve atmosphereCurve;
@@ -59,20 +66,28 @@ namespace KerbalWindTunnel.VesselCache
         public static SimulatedEngine Borrow(ModuleEngines module, SimulatedPart part)
         {
             SimulatedEngine engine = pool.Borrow();
+            engine.vessel = part.vessel;
             engine.Init(module, part);
             return engine;
         }
 
         protected void Init(ModuleEngines engine, SimulatedPart part)
         {
+            this.flameoutBar = engine.flameoutBar;
             this.atmChangeFlow = engine.atmChangeFlow;
             this.useAtmCurve = engine.useAtmCurve;
+            this.useAtmCurveIsp = engine.useAtmCurveIsp;
             this.atmCurve = new FloatCurve(engine.atmCurve.Curve.keys);
+            this.atmCurveIsp = new FloatCurve(engine.atmCurveIsp.Curve.keys);
             this.useVelCurve = engine.useVelCurve;
+            this.useVelCurveIsp = engine.useVelCurveIsp;
             this.velCurve = new FloatCurve(engine.velCurve.Curve.keys);
+            this.velCurveIsp = new FloatCurve(engine.velCurveIsp.Curve.keys);
             this.flowMultCap = engine.flowMultCap;
             this.flowMultCapSharpness = engine.flowMultCapSharpness;
             this.atmosphereCurve = new FloatCurve(engine.atmosphereCurve.Curve.keys);
+            this.useThrustCurve = engine.useThrustCurve;
+            this.thrustCurve = new FloatCurve(engine.thrustCurve.Curve.keys);
             this.useThrottleIspCurve = engine.useThrottleIspCurve;
             this.throttleIspCurve = new FloatCurve(engine.throttleIspCurve.Curve.keys);
             this.throttleIspCurveAtmStrength = new FloatCurve(engine.throttleIspCurveAtmStrength.Curve.keys);
@@ -94,38 +109,66 @@ namespace KerbalWindTunnel.VesselCache
 
         public Vector3 GetThrust(float mach, float atmDensity, float atmPressure, bool oxygenPresent)
         {
+            return GetThrust(mach, atmDensity, atmPressure, oxygenPresent, out _);
+        }
+        public Vector3 GetThrust(float mach, float atmDensity, float atmPressure, bool oxygenPresent, out float fuelBurnRate)
+        {
+            atmPressure *= 0.00986923267f;
+            fuelBurnRate = 0;
             if (requiresOxygen && !oxygenPresent)
                 return Vector3.zero;
-            Vector3 thrust = thrustVector;
-            float thrustLevel = 1;
-            if (atmChangeFlow)
-            {
-                if (useAtmCurve)
-                    lock (this.atmCurve)
-                        thrustLevel *= atmCurve.Evaluate(atmDensity * (40f / 49f));
-                else
-                    thrustLevel *= atmDensity * (40f / 49f);
-            }
-            if (useVelCurve)
-                lock (this.velCurve)
-                    thrustLevel *= velCurve.Evaluate(mach);
-            if (thrustLevel > flowMultCap)
-                thrustLevel = flowMultCap + (thrustLevel - flowMultCap) / (flowMultCapSharpness + thrustLevel / flowMultCap - 1);
-            thrustLevel = Mathf.Max(thrustLevel, CLAMP);
 
+            fuelBurnRate = GetFuelBurnRate(mach, atmDensity);
+            if (fuelBurnRate <= 0)
+                return Vector3.zero;
+            
             float isp = 0;
-            lock (this.atmosphereCurve)
+            lock (atmosphereCurve)
                 isp = atmosphereCurve.Evaluate(atmPressure);
             if (useThrottleIspCurve)
                 lock (throttleIspCurve)
                     isp *= Mathf.Lerp(1f, throttleIspCurve.Evaluate(1), throttleIspCurveAtmStrength.Evaluate(atmPressure));
+            if (useAtmCurveIsp)
+                lock (atmCurveIsp)
+                    isp *= atmCurveIsp.Evaluate(atmDensity * 40 / 49);
+            if (useVelCurveIsp)
+                lock (velCurveIsp)
+                    isp *= velCurveIsp.Evaluate(mach);
 
-            return thrust * g * multIsp * maxFuelFlow * multFlow * (thrustPercentage / 100f) * isp * thrustLevel;
+
+            if (!requiresOxygen)
+                Debug.Log(string.Format("Fuel: {0}, ISP: {1}, Thrust: {2}", fuelBurnRate, isp, fuelBurnRate * g * multIsp * thrustPercentage / 100f));
+            return thrustVector * fuelBurnRate * g * multIsp * isp * (thrustPercentage / 100f);
         }
 
-        public float GetFuelBurnRate(float mach, float atmDensity, float atmPressure, bool oxygenPresent)
+        public float GetFuelBurnRate(float mach, float atmDensity)
         {
-            throw new NotImplementedException();
+            float flowMultiplier = 1;
+            if (atmChangeFlow)
+            {
+                if (useAtmCurve)
+                    lock (atmCurve)
+                        flowMultiplier = atmCurve.Evaluate(atmDensity * 40 / 49);
+                else
+                    flowMultiplier = atmDensity * 40 / 49;
+            }
+            if (useThrustCurve)
+                lock (thrustCurve)
+                    flowMultiplier *= thrustCurve.Evaluate(1f);
+            if (useVelCurve)
+                lock (velCurve)
+                    flowMultiplier *= velCurve.Evaluate(mach);
+            if (flowMultiplier > flowMultCap)
+            {
+                float excess = flowMultiplier - flowMultCap;
+                flowMultiplier = flowMultCap + excess / (flowMultCapSharpness + excess / flowMultCap);
+            }
+            if (flowMultiplier < CLAMP)
+                flowMultiplier = CLAMP;
+
+            if (flowMultiplier < flameoutBar)
+                return 0;
+            return flowMultiplier * maxFuelFlow * multFlow;
         }
     }
 }
