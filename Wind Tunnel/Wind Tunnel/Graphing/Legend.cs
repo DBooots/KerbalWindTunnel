dc7260cd9d54c9436b97c6c8fdce12bd085c4d89
David <david.boots@gmail.com>
2018-10-14 13:46:05 -0500

    Add the basis for a Legend object to Graphing.

---
+++ Wind Tunnel/Wind Tunnel/Graphing/Legend.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KerbalWindTunnel.Graphing
+{
+    public class Legend
+    {
+        public static readonly Color[] Bright = new Color[]
+            { Color.green, Color.yellow, Color.red, Color.cyan, new Color(148, 0, 211), new Color(255, 140, 0), Color.blue };
+        private Grapher grapher;
+        private Color[] colors = Bright;
+
+        public Legend(Grapher grapher)
+        {
+            this.grapher = grapher;
+        }
+
+        public void SetColors()
+        {
+            int colorIndex = 0;
+            for (int i = 0; i < grapher.Count; i++)
+            {
+                if (grapher[i] is ILineGraph lineGraph)
+                {
+                    lineGraph.Color = this.colors[colorIndex % colors.Length];
+                    colorIndex++;
+                }
+            }
+        }
+    }
+}
