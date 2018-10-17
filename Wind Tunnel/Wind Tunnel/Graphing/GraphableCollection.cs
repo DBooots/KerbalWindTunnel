7c332ac4a9bcad16f5fbaf3c67365459f73105f8
David <david.boots@gmail.com>
2018-10-16 21:16:51 -0500

    Fix bug with GraphableCollection Visibility

--- Wind Tunnel/Wind Tunnel/Graphing/GraphableCollection.cs
+++ Wind Tunnel/Wind Tunnel/Graphing/GraphableCollection.cs
@@ -16,10 +16,7 @@ namespace KerbalWindTunnel.Graphing
                 bool changed = _visible != value;
                 _visible = value;
                 if (changed)
-                {
                     OnValuesChanged(null);
-                    SetVisibility(value);
-                }
             }
         }
         private bool _visible = true;
