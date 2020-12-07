5a0f32ceea99d38b150fcd9fd9ba158874842938
David <david.boots@gmail.com>
2020-12-07 16:35:54 -0600

    Improve logging and error catching of reflection access to FAR.

--- Wind Tunnel/Wind Tunnel/FARVesselCache/FARHook.cs
+++ Wind Tunnel/Wind Tunnel/FARVesselCache/FARHook.cs
@@ -32,45 +32,61 @@ namespace KerbalWindTunnel.FARVesselCache
             FARassembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.FullName.Contains(assemblyName));
             if (FARassembly == null)
             {
-                UnityEngine.Debug.Log("FAR not installed");
+                UnityEngine.Debug.Log("Kerbal Wind Tunnel - FAR not installed");
                 installed = false;
                 initiated = true;
                 return;
             }
+            else
+                UnityEngine.Debug.Log("Kerbal Wind Tunnel - FAR is installed.");
             installed = true;
 
             simulationType = FARassembly.GetTypes().FirstOrDefault(t => t.Name.Contains("InstantConditionSimulation"));
             simulationOutputType = FARassembly.GetTypes().FirstOrDefault(t => t.Name.Contains("InstantConditionSimOutput"));
             if (simulationType == null || simulationOutputType == null)
             {
-                UnityEngine.Debug.Log("FAR API type not found!");
+                UnityEngine.Debug.LogError("Kerbal Wind Tunnel - FAR API type not found!");
+                if (simulationType == null)
+                    UnityEngine.Debug.Log("Could not find 'InstandConditionSimulation' Type.");
+                if (simulationOutputType == null)
+                    UnityEngine.Debug.Log("Could not find 'InstantConditionSimOutput' Type.");
                 installed = false;
                 initiated = true;
                 return;
             }
 
-            UpdateCurrentBody = (Action<CelestialBody>)Delegate.CreateDelegate(typeof(Action<CelestialBody>), FARassembly.GetTypes().FirstOrDefault(t => t.Name.Contains("FARAPI")).GetNestedType("Simulation").GetMethod("UpdateCurrentBody"));
+            try
+            {
+                UpdateCurrentBody = (Action<CelestialBody>)Delegate.CreateDelegate(typeof(Action<CelestialBody>), FARassembly.GetTypes().FirstOrDefault(t => t.Name.Contains("FARAPI")).GetNestedType("Simulation").GetMethod("UpdateCurrentBody"));
 
-            InstantConditionSimulationWrapper._constructor = simulationType.Constructor();
-            InstantConditionSimulationWrapper._iterationOutput = simulationType.PropertyGet<object>("iterationOutput");
-            InstantConditionSimulationWrapper._ready = simulationType.PropertyGet<bool>("Ready");
-            InstantConditionSimulationWrapper._calculateAccelerationDueToGravity = simulationType.InstanceMethod<Func<object, CelestialBody, double, double>>("CalculateAccelerationDueToGravity");
-            //InstantConditionSimulationWrapper._computeNonDimensionalForcesO = simulationType.InstanceMethod<Func<object, object, bool, bool, object>>("ComputeNonDimensionalForces");
-            InstantConditionSimulationWrapper._computeNonDimensionalForcesS = simulationType.InstanceMethod<Func<object, double, double, double, double, double, double, double, double, bool, bool, object>>("ComputeNonDimensionalForces");
-            InstantConditionSimulationWrapper._computeNonDimensionalForcesE = simulationType.InstanceMethod<Func<object, double, double, double, double, double, double, double, double, int, bool, bool, bool, object>>("ComputeNonDimensionalForces");
-            //InstantConditionSimulationWrapper._setState = simulationType.InstanceMethod<Action<object, double, double, Vector3d, double, int, bool>>("SetState");
-            InstantConditionSimulationWrapper._functionIterateForAlpha = simulationType.InstanceMethod<Func<object, double, double>>("FunctionIterateForAlpha");
-            //InstantConditionSimulationWrapper._computeRequiredAoA = simulationType.InstanceMethod<Func<object, double, double, Vector3d, double, int, bool, double>>("ComputeRequiredAoA");
-            InstantConditionSimulationWrapper._update = simulationType.InstanceMethod<Action<object>>("Update");
+                InstantConditionSimulationWrapper._constructor = simulationType.Constructor();
+                InstantConditionSimulationWrapper._iterationOutput = simulationType.FieldGet<object>("iterationOutput");
+                InstantConditionSimulationWrapper._ready = simulationType.PropertyGet<bool>("Ready");
+                InstantConditionSimulationWrapper._calculateAccelerationDueToGravity = simulationType.InstanceMethod<Func<object, CelestialBody, double, double>>("CalculateAccelerationDueToGravity");
+                //InstantConditionSimulationWrapper._computeNonDimensionalForcesO = simulationType.InstanceMethod<Func<object, object, bool, bool, object>>("ComputeNonDimensionalForces");
+                InstantConditionSimulationWrapper._computeNonDimensionalForcesS = simulationType.InstanceMethod<Func<object, double, double, double, double, double, double, double, double, bool, bool, object>>("ComputeNonDimensionalForces");
+                InstantConditionSimulationWrapper._computeNonDimensionalForcesE = simulationType.InstanceMethod<Func<object, double, double, double, double, double, double, double, double, int, bool, bool, bool, object>>("ComputeNonDimensionalForces");
+                //InstantConditionSimulationWrapper._setState = simulationType.InstanceMethod<Action<object, double, double, Vector3d, double, int, bool>>("SetState");
+                InstantConditionSimulationWrapper._functionIterateForAlpha = simulationType.InstanceMethod<Func<object, double, double>>("FunctionIterateForAlpha");
+                //InstantConditionSimulationWrapper._computeRequiredAoA = simulationType.InstanceMethod<Func<object, double, double, Vector3d, double, int, bool, double>>("ComputeRequiredAoA");
+                InstantConditionSimulationWrapper._update = simulationType.InstanceMethod<Action<object>>("Update");
 
-            InstantConditionSimOutputWrapper._getCl = simulationOutputType.FieldGet<double>("Cl");
-            InstantConditionSimOutputWrapper._getCd = simulationOutputType.FieldGet<double>("Cd");
-            InstantConditionSimOutputWrapper._getCm = simulationOutputType.FieldGet<double>("Cm");
-            InstantConditionSimOutputWrapper._getCy = simulationOutputType.FieldGet<double>("Cy");
-            InstantConditionSimOutputWrapper._getCn = simulationOutputType.FieldGet<double>("Cn");
-            InstantConditionSimOutputWrapper._getC_roll = simulationOutputType.FieldGet<double>("C_roll");
-            InstantConditionSimOutputWrapper._getArea = simulationOutputType.FieldGet<double>("area");
-            InstantConditionSimOutputWrapper._getMAC = simulationOutputType.FieldGet<double>("MAC");
+                InstantConditionSimOutputWrapper._getCl = simulationOutputType.FieldGet<double>("Cl");
+                InstantConditionSimOutputWrapper._getCd = simulationOutputType.FieldGet<double>("Cd");
+                InstantConditionSimOutputWrapper._getCm = simulationOutputType.FieldGet<double>("Cm");
+                InstantConditionSimOutputWrapper._getCy = simulationOutputType.FieldGet<double>("Cy");
+                InstantConditionSimOutputWrapper._getCn = simulationOutputType.FieldGet<double>("Cn");
+                InstantConditionSimOutputWrapper._getC_roll = simulationOutputType.FieldGet<double>("C_roll");
+                InstantConditionSimOutputWrapper._getArea = simulationOutputType.FieldGet<double>("area");
+                InstantConditionSimOutputWrapper._getMAC = simulationOutputType.FieldGet<double>("MAC");
+
+            }
+            catch (Exception ex)
+            {
+                UnityEngine.Debug.LogError("Kerbal Wind Tunnel - Encountered exception on loading FAR hooks:");
+                UnityEngine.Debug.LogException(ex);
+                installed = false;
+            }
 
             initiated = true;
         }
